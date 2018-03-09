6f284165446981fe267f2c411aa20c3b5162486a
Dianne <diannehodnett@gmail.com>
2018-03-08 20:00:41 -0700

    initial commit

---
+++ DianneJS.js
@@ -0,0 +1,42 @@
+console.log("Hello World")
+//Adding a String and a Number
+// const a = 4
+// const b = 2
+// const c = a + b
+// console.log(c)
+
+//#2 in the Assignment
+// const a = 2
+// const b = "Dianne"
+// const c = a + b
+// console.log(c)
+
+//# 5 in the Assignment
+// let myName = "Dianne"
+//
+// let myLastName = " Hodnett"
+//
+// let fullName = myName + myLastName
+// console.log(fullName)
+
+//Boolean Example
+// const great = 4
+//
+// if (great > 5){
+//     console.log("pass")
+// } else {
+//     console.log("fail")
+// }
+
+
+// this my array #6
+//
+// let lego = ["pointy", "sharp", "flat", "thin"];
+// lego[6] = "pick them up";
+//
+// console.log(lego)
+
+// object literal with 3 key value pairs
+
+// let car = {make: 'Acura', year: 2017, color:'black'};
+// console.log(car);
